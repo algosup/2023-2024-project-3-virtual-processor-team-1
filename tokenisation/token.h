1d3e41f15690ca814f5655a63db925cc9ed6ecc7
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2024-01-29 16:57:31 +0100

    Update token.h

---
+++ tokenisation/token.h
@@ -0,0 +1,70 @@
+typedef enum instruction
+{
+    MOV,
+    ADD, 
+    SUB, 
+    MUL,
+    DIV,
+    JMP,
+    JE,
+    JNE,
+    JG,
+    JGE,
+    JL,
+    JLE,
+    RET,
+    CMP,
+    CALL,
+    AND,
+    OR,
+    XOR,
+    NOT,
+    GAD,
+    DISP,
+} instruction_t;
+
+typedef enum registers
+{
+    R1,
+    R2,
+    R3,
+    R4,
+
+} registers_t;
+
+typedef enum address_registers
+{
+    A1,
+    A2,
+    A3,
+    A4,
+
+} address_registers_t;
+
+typedef enum values
+{
+    IM,
+    NE,
+    STR
+
+} values_t;
+
+typedef struct token
+{
+    data_t type; // register, operator, address register, value
+    data_t value; // value of the token (register, operator, address register, value)
+    data_t line; // line number 
+    data_t column; // column number
+
+}token_t;
+
+typedef union data
+{
+    instruction_t instruction;
+    registers_t registers;
+    address_registers_t address_registers;
+    values_t values;
+} data_t;
+
+
+
