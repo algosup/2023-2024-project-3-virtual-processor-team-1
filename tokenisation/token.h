adcc662d054cc82e0eed3dcfb330594da52c076a
DELILLE Elone <147847949+HiNett@users.noreply.github.com>
2024-02-01 09:40:53 +0100

    Update the Tokenization System
    Review of the tokenisation system: from now on, the tokenisation system will work on several lines, and with most of the instructions.

--- tokenisation/token.h
+++ tokenisation/token.h
@@ -66,70 +66,143 @@ typedef enum typeofdata
 
 } typeofdata_t;
 
-typedef struct token
-{
-    typeofdata_t type; // register, operator, address register, value
-    typeofdata_t parameter; // parameter of the token (register, operator, address register, value)
-    int row; // row number 
-    int column; // column number
-
-}token_t;
+typedef struct token {
+    char type[20];    // String representation of the type
+    char value[20];   // String representation of the value
+    int row;          // Row number 
+    int column;       // Column number
+} token_t;
 
 int column = 0;
 int row = 0;
 
-void getEnum(char cleanedLines[1000], token_t *token){      // This function permitte to find the type of instruction
-    if (strcmp(cleanedLines, "MOV") == 0){
-        token->type = INSTRUCTION;
-        token->parameter = MOV;
-    } else {
-        token->type = VALUE;
-        token->parameter = A1;
+void getEnum(char cleanedLines[1000], token_t *token) {
+    // Instructions
+    if (strcmp(cleanedLines, "MOV") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "MOV");
+    } else if (strcmp(cleanedLines, "ADD") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "ADD");
+    } else if (strcmp(cleanedLines, "SUB") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "SUB");
+    } else if (strcmp(cleanedLines, "MUL") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "MUL");
+    } else if (strcmp(cleanedLines, "DIV") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "DIV");
+    } else if (strcmp(cleanedLines, "JMP") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JMP");
+    } else if (strcmp(cleanedLines, "JE") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JE");
+    } else if (strcmp(cleanedLines, "JNE") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JNE");
+    } else if (strcmp(cleanedLines, "JG") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JG");
+    } else if (strcmp(cleanedLines, "JGE") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JGE");
+    } else if (strcmp(cleanedLines, "JL") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JL");
+    } else if (strcmp(cleanedLines, "JLE") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "JLE");
+    } else if (strcmp(cleanedLines, "RET") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "RET");
+    } else if (strcmp(cleanedLines, "CMP") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "CMP");
+    } else if (strcmp(cleanedLines, "CALL") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "CALL");
+    } else if (strcmp(cleanedLines, "AND") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "AND");
+    } else if (strcmp(cleanedLines, "OR") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "OR");
+    } else if (strcmp(cleanedLines, "XOR") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "XOR");
+    } else if (strcmp(cleanedLines, "NOT") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "NOT");
+    } else if (strcmp(cleanedLines, "GAD") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "GAD");
+    } else if (strcmp(cleanedLines, "DISP") == 0) {
+        strcpy(token->type, "instruction");
+        strcpy(token->value, "DISP");
+    } 
+    else if (strcmp(cleanedLines, "R1") == 0) {
+        strcpy(token->type, "register");
+        strcpy(token->value, "R1");
+    } else if (strcmp(cleanedLines, "R2") == 0) {
+        strcpy(token->type, "register");
+        strcpy(token->value, "R2");
+    } else if (strcmp(cleanedLines, "R3") == 0) {
+        strcpy(token->type, "register");
+        strcpy(token->value, "R3");
+    } else if (strcmp(cleanedLines, "R4") == 0) {
+        strcpy(token->type, "register");
+        strcpy(token->value, "R4");
+    } else if (strcmp(cleanedLines, "A1") == 0) {
+        strcpy(token->type, "address_register");
+        strcpy(token->value, "A1");
+    } else if (strcmp(cleanedLines, "A2") == 0) {
+        strcpy(token->type, "address_register");
+        strcpy(token->value, "A2");
+    } else if (strcmp(cleanedLines, "A3") == 0) {
+        strcpy(token->type, "address_register");
+        strcpy(token->value, "A3");
+    } else if (strcmp(cleanedLines, "A4") == 0) {
+        strcpy(token->type, "address_register");
+        strcpy(token->value, "A4");
+    } else if (cleanedLines[0] == '-') {
+        strcpy(token->type, "NE");
+        strcpy(token->value, cleanedLines);
+    } else if (cleanedLines[0] == '"') {
+        strcpy(token->type, "STR");
+        strcpy(token->value, cleanedLines);
+    }
+    else if (cleanedLines[0] == '#') {
+        strcpy(token->type, "immediate");
+        strcpy(token->value, cleanedLines);
+    }
+
+    // Default case if none matched
+    else {
+        strcpy(token->type, "void");
+        strcpy(token->value, "void");
     }
 }
 
+
+
 // Output:
 // {Token("instruction", "MOV", 1, 1), Token("register", "R1", 1, 2), Token("immediate", "#5", 1, 3)}
-void tokenizationFunction(char cleanedLines[][3][1000], int numLines){
-    for(int i = 0; i < numLines; i++){
-        int stockedFirstType;
-        int stockedFirstParameter;
-        int stockedSecondType;
-        int stockedSecondParameter;
-        int stockedThirdType;
-        int stockedThirdParameter;
-        for(int c = 0; c < 3; c++){
-            switch(c){
-                token_t token;
-                case 0:  
-                    token.row = i;
-                    token.column = c;
-                    getEnum(cleanedLines[c][i], &token);
-                    stockedFirstType = token.type;
-                    stockedFirstParameter = token.parameter;
-                    break;
-                case 1:
-                    token.row = i;
-                    token.column = c;
-                    getEnum(cleanedLines[c][i], &token);
-                    stockedSecondType = token.type;
-                    stockedSecondParameter = token.parameter;
-                    break;
-                case 2:
-                    token.row = i;
-                    token.column = c;
-                    getEnum(cleanedLines[c][i], &token);
-                    stockedThirdType = token.type;
-                    stockedThirdParameter = token.parameter;
-                    break;
-                default:
-                    printf("ERROR");
-                    break;
+void tokenizationFunction(char cleanedLines[][3][1000], int numLines) {
+    for (int i = 0; i < numLines; i++) {
+        for (int c = 0; c < 3; c++) {
+            token_t token;
+            token.row = i + 1;  // Rows and columns should start from 1 for this output format
+            token.column = c + 1;
+            getEnum(cleanedLines[i][c], &token);
+
+            // Output the token information
+            printf("Token(\"%s\", \"%s\", %d, %d)", token.type, token.value, token.row, token.column);
+            if (c < 2) {
+                printf(", ");
             }
-            // printf("Line %d\nColumn %d\nType %d\nParameter %d\n\n", token.row, token.column, token.type, token.parameter);
         }
-        printf("1. Type %d\nParameter %d", stockedFirstType, stockedFirstParameter);
-        printf("\n2. Type %d\nParameter %d", stockedSecondType, stockedSecondParameter);
-        printf("\n3. Type %d\nParameter %d", stockedThirdType, stockedThirdParameter);
+        printf("\n");
     }
 }
